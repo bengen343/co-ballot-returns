be35ddc88f9acf3a9de424c7f1998d290125f898
Benjamin Engen <benjamin.engen@gmail.com>
2022-06-15 09:50:12 +0200

    finalized main.py

--- main.py
+++ main.py
@@ -78,6 +78,3 @@ def main():
 if __name__ == '__main__':
     from waitress import serve
     serve(app, host='0.0.0.0', port=8080)
-
-#if __name__ == '__main__':
-#    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
