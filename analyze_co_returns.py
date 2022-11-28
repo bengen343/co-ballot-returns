c305db5eaf0572726b4bc5baf78e3475f894421c
Benjamin Engen <benjamin.engen@gmail.com>
2022-11-28 10:46:33 +0000

    refactor: multiprocessing code for concision

--- analyze_co_returns.py
+++ analyze_co_returns.py
@@ -23,15 +23,14 @@ def vertical_crosstab(vertical: str, crosstab_criteria_lst: list, df: pd.DataFra
 
 
 def async_crosstabs(crosstab_criteria_lst: list, df: pd.DataFrame):
-    print(f"Starting crosstab calculation.")
+    processes_int = len(os.sched_getaffinity(0))
     crosstabs_df = pd.DataFrame()
-
-    pool = multiprocessing.Pool()
     vertical_crosstab_lst = crosstab_criteria_lst + ['PRECINCT']
+    print(f"Starting crosstab calculation with {processes_int} workers.")
     
-    results = [pool.apply_async(vertical_crosstab, args=(x, crosstab_criteria_lst, df)) for x in vertical_crosstab_lst]
-    pool.close()
-    pool.join()
-    crosstabs_df = pd.concat([p.get() for p in results], axis=0, sort=False)
+    with multiprocessing.Pool(processes=processes_int) as pool:
+    
+        results = [pool.apply_async(vertical_crosstab, args=(x, crosstab_criteria_lst, df)) for x in vertical_crosstab_lst]
+        crosstabs_df = pd.concat([p.get() for p in results], axis=0, sort=False)
     
     return crosstabs_df
